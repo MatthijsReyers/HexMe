9b02c5d92dd5011bccdfd0c5d7f238f368591c1f
MatthijsReyers <matthijscrafter@gmail.com>
2019-09-08 18:25:49 +0200

    1.1 update

--- source/argparser.h
+++ source/argparser.h
@@ -27,7 +27,8 @@ void showHelp()
     std::cout << "-n                Do not show line numbers." << std::endl;
     std::cout << "-c [int]          How many collums to use for listing" << std::endl;
     std::cout << "                  bytes, (default = 2, max = 4)." << std::endl;
-    
+
+
     exit(0);
 }
 
