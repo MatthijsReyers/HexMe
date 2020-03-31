522d6fea74c44c765d8d2b64aa00cf51dbcbd1af
matthijsreyers <matthijscrafter@gmail.com>
2020-03-31 23:26:17 +0200

    updates

--- release/3.0/install.sh
+++ release/3.0/install.sh
@@ -1,4 +1,8 @@
-echo "Installing HexMe version 1.2"
-sudo rm /usr/bin/hexme > /dev/null 2>&1
-sudo cp hexme /usr/bin
-echo "Finished installing..."
+if [[ $EUID -eq 0 ]]; then 
+    echo "Installing HexMe version 3.0"
+    sudo rm /usr/bin/hexme > /dev/null 2>&1
+    sudo cp hexme /usr/bin
+    echo "Finished installing..."
+else
+    echo "Please run as root"
+fi
